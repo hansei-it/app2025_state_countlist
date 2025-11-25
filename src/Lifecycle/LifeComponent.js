da310d4c7fb4ed2765b1a53bbffead42c8f354cc
DESKTOP-RUCRN6V\winlock <armyvision@hansei.ac.kr>
2025-11-25 11:55:59 +0900

    [add] 컴포넌트 마운트,언마운트,업데이트 확인 전체 기능 확인을 위한 기능 추가

--- src/Lifecycle/LifeComponent.js
+++ src/Lifecycle/LifeComponent.js
@@ -1,7 +1,12 @@
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function LifeComponent() {
+  const [text, setText] = useState('hello');
+  const [count, setCount] = useState(0);
+
+  console.log('=== 컴포넌트가 랜더링 되었습니다. ===')
+  
   useEffect(() => {
     console.log(" 첫 컴포넌트 마운트(Lifecycle) !");
 
@@ -10,9 +15,20 @@ function LifeComponent() {
     }
   }, []);
 
+  useEffect(() => {
+    console.log("[update] 첫 컴포넌트 마운트와 컴포넌트 업데이트 마다 실행");    
+  });
+
+  useEffect(() => {
+    console.log("[update] count 상태 업데이트 시에 실행");    
+  },[count]);
+
   return (
-    <div>
-      LifeComponent
+    <div>      
+      LifeComponent : <input type='text' value={text} onChange={(e)=>setText(e.target.value)}/>
+      <div></div>
+      <div><button onClick={()=>setCount(count+1)}> + </button> <button onClick={()=>setCount(count-1)}> - </button></div>
+      Count : {count}
     </div>
   );
 }
