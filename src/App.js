667bbf85119361fee57bca162e9c7b3920ab1d0c
DESKTOP-RUCRN6V\winlock <armyvision@hansei.ac.kr>
2025-11-25 11:01:01 +0900

    [update] App 컴포넌트의 state 훅 상태 코드를 reducer를 사용하도록  모두 수정.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function reducer(state, action)
   {
     case 'ADD':
       return [...state, action.item];
+    case 'UPDATE':
+      return state.map((v)=> v.id===action.item.id ? action.item: v);
+    case 'DELETE':
+      return state.filter((v)=> v.id !== action.item.id);
     default:
       return state;
   }
@@ -19,7 +23,7 @@ function getRandColor()
 function App() {
   const [countState, dispatch] = useReducer(reducer, [{num:10,color:'#FF00F0',id:1},{num:13,color:'#F0FEA0',id:2}]);
   const countRef = useRef(2);
-  
+
   const onAddList = (number)=>
   {
     dispatch( {type:'ADD', item:{num:number, color:getRandColor(), id:++countRef.current}});
